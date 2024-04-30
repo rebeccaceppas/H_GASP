57977c7ad640aee995015f1f4096a08d6d76f4d3
Rebecca Ceppas de Castro <rebecca@ceppas.co>
2024-04-30 07:25:04 -0700

    improved efficiency of sample drawing

--- error_sampling.py
+++ error_sampling.py
@@ -1,24 +1,37 @@
+# Code developed with Olivia Pereira #
+
 import numpy as np
 from scipy import integrate
 
-# helper function
-def draw_sample(bins, heights, n):
-    # cumulatively integrating 
-    y_cumulative = integrate.cumtrapz(heights, initial = 0)
-    draws = []
-
+def draw_sample(x, y, n):
+    ''' Sampling function to draw n samples from a distribution y(x) '''
+    y_cumulative = integrate.cumtrapz(y, initial = 0)
+    
     # randomly sampling based on original probability
-    for i in range(n):
-        choice = np.random.uniform(low = 0, high = 1)
-        draw = np.interp(choice, y_cumulative, bins)
-        draws.append(draw)
-
-    return np.array(draws)
+    choices = np.random.uniform(low=0, high=1, size=n)
+    draws = np.interp(choices, y_cumulative, x)
+    return draws
 
 
-# wrapper function
+################### wrapper functions for visibility calibration errors ###################
+
 def draw_random_errors(phase_bins, phase_heights, amp_bins, amp_heights, output_shape):
+    ''' 
+    Draws phase and amplitude errors from given error distributions 
+    
+    Inputs
+    - phase_bins, amp_bins: <array>
+      x-values on which the error distributions for phase and amplitude are given
+    - phase_heights, amp_heights: <array>
+      y-values of the error distribution for phase and amplitude
+    - output_shape: <tuple>
+      desired shape of the output errors
 
+    Outputs
+    - phase_draws, amp_draws: <array>
+      phase and amplitude errors sampled from the distribution
+      shape is the requeste output_shape
+    '''
     # normalizing the input heights to make it into a probability distribution
     phase_heights = phase_heights / integrate.trapz(np.abs(phase_heights))
     amp_heights = amp_heights / integrate.trapz(np.abs(amp_heights))
@@ -33,14 +46,26 @@ def draw_random_errors(phase_bins, phase_heights, amp_bins, amp_heights, output_
     return phase_draws, amp_draws
 
 
-# wrapper function for once we have the file 
-def get_calibration_errors(path_to_files, output_shape):
+def get_calibration_errors(amplitude_path, phase_path, output_shape):
+    '''
+    Loads the amplitude and phase error distributions from .npy files
+    Draws the error samples and creates a complex gain factor that can be applied to 
+    visibilities to introduce the desired phase errors and percent amplitude errors
+    
+    Inputs
+    - amplitude_path, phase_path: <str>
+      file paths for amplitude and phase error distributions
+    - output_shape: <tuple>
+      desired shape of the output gain factor
 
-    amplitude_file = path_to_files + 'visibility_amplitude_errors.npy'
-    phase_file = path_to_files + 'visibility_phase_errors.npy'
+    Output
+    - G: <array>
+      complex error gain factor to apply to the visibilities
+    '''
 
-    amp = np.load(amplitude_file)
-    phase = np.load(phase_file)
+
+    amp = np.load(amplitude_path)
+    phase = np.load(phase_path)
     percentages, amp_errors = amp[:,0], amp[:,1]
     radians, phase_errors = phase[:,0], phase[:,1]
     
