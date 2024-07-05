f0bdbdcac480edb9a34d55860e6b1a7f2f50854b
Rebecca Ceppas de Castro <rebecca@ceppas.co>
2024-07-05 07:00:34 -0700

    config changing not working

--- notebooks/beam_transfer_matrices.py
+++ notebooks/beam_transfer_matrices.py
@@ -9,17 +9,24 @@ fmax = 1420
 fmin = 1419
 U = 4
 
-f_start, f_end, nfreq = fr.get_frequencies(fmax, fmin, U)
+print('setting up frequencies')
+fstate, f_start, f_end, nfreq = fr.get_frequencies(fmax, fmin, U)
 
-output_directory = './'
+output_directory = './beams'
 
 CHORDdec_pointing = 10
 n_dishes_ew = 2
 n_dishes_ns = 2
 
-
+print('initializing object')
 btm = obs.BeamTransferMatrices(f_start, f_end, nfreq, output_directory,
                                CHORDdec_pointing, n_dishes_ew=n_dishes_ew,
                                n_dishes_ns=n_dishes_ns)
 
-btm.get_beam_transfer_matrices()
+
+
+btm.change_config()
+#print('getting matrices')
+#btm.get_beam_transfer_matrices()
+
+print('done')
