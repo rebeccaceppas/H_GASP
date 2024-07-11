916489d8e46c12784ee44b62293b3e8448c96075
Rebecca Ceppas de Castro <rebecca@ceppas.co>
2024-07-11 13:15:25 -0700

    full run set up

--- tutorials/full_run.py
+++ tutorials/full_run.py
@@ -8,52 +8,51 @@ module load chord/chord_pipeline/2023.06
 
 import sys
 
-sys.path.append('/home/rebeccac/scratch/H-GASP')
+sys.path.append('/home/rebeccac/scratch/H_GASP')
 
-import input_maps as im
-import observations as obs
-import frequencies as fr
+from H_GASP import input_maps as im
+from H_GASP import observations as obs
+from H_GASP import frequencies as fr
 
-############# setting up the basic parameters for the run ##########
+####### setting up the basic parameters for the run #######
 fmax = 1420
 fmin = 1415
-nfreq  = 40
+nfreq  = 40  # this is the frequency of the map creation
+             # if you are going to up-channelize the map, you can keep this
+             # large enough so the shapes of the profiles are not lost pre upchan
 nside = 128
-output_directory = '/home/rebeccac/scratch/H-GASP/notebooks/full_run/'
+output_directory = '/home/rebeccac/scratch/H_GASP/tutorials/full_run/'
 
 U = 2
 
-compute_R = False
+compute_R = True  # whether you need to compute R and norm for up-channelization
+                  # if they have already been compute this script will just straight
+                  # to performing the up-channelization
 
 CHORDdec_pointing = 10  # declination in degrees
 n_dishes_ew = 2
 n_dishes_ns = 1
-# these are the CHORD pathfinder defaults but I'm explicitely using them to show it can be changed
 spacing_ew = 6.3
 spacing_ns = 8.5
 dish_diameter = 6.0
 
+# for adding noise
 ndays = 100
 
-
-############## creating input maps: one with all galaxies in the galaxy catalog and one with cora-makesky foregrounds ##############
-
-print('creating maps')
-
+###### creating input maps: one with all galaxies in the galaxy catalog and one with cora-makesky foregrounds ######
 catalog_filepath = './HI_Catalog.txt'
 HIgals_filename = 'HI_gals.h5'
-#hi_gals = im.HIGalaxies(catalog_filepath, fmax, fmin, nfreq)
-#hi_gals.get_map(nside, output_directory+HIgals_filename)
+hi_gals = im.HIGalaxies(catalog_filepath, fmax, fmin, nfreq)
+hi_gals.get_map(nside, output_directory+HIgals_filename)
 
 
 components = ['foreground', '21cm']
-#fg = im.Foregrounds(fmax, fmin, nfreq, nside, output_directory)
-#fg.get_maps(components)
+fg = im.Foregrounds(fmax, fmin, nfreq, nside, output_directory)
+fg.get_maps(components)
 
 ############## up-channelizing the maps ################
 
-print('upchannelizing')
-
+# list of all the maps to up-channelize
 map_paths = [output_directory+HIgals_filename,
              output_directory+'foregrounds_all.h5']
 
@@ -70,9 +69,8 @@ upchan = obs.Upchannelization(U,
                               R_filename,
                               norm_filename,
                               freq_matrix_filename)
-
-#if compute_R:
-#    upchan.get_R_norm()
+if compute_R:
+    upchan.get_R_norm()
 
 f_start, f_end, nfreqU = upchan.upchannelize(map_paths=map_paths)
 
@@ -80,34 +78,27 @@ fstate = fr.FreqState()
 fstate.freq = (f_start, f_end, nfreqU)
 
 ############# computing beam transfer matrices #####################
-
-print('getting btm')
-
+# choosing the save the matrices in the output directory but can decide on something else
 btm_directory = output_directory
 
-#btm = obs.BeamTransferMatrices(f_start, f_end, nfreqU, output_directory, CHORDdec_pointing,
-                               #n_dishes_ew, n_dishes_ns, spacing_ew, spacing_ns,
-                               #dish_diameter, ndays=ndays)
+btm = obs.BeamTransferMatrices(f_start, f_end, nfreqU, output_directory, CHORDdec_pointing,
+                               n_dishes_ew, n_dishes_ns, spacing_ew, spacing_ns,
+                               dish_diameter, ndays=ndays)
 
-#btm.get_beam_transfer_matrices()
+btm.get_beam_transfer_matrices()
 
 
 ############# getting visibilities ##################
-
-print('getting vis')
-
-maps_tag = 'HIcatalog_foregrounds'  # a tag to remember which components were in the iput maps
+maps_tag = 'HIcatalog_foregrounds'  # a single tag to remember which components were in the input maps
 map_filepaths = [output_directory+upchan_filename]
 
-#vis = obs.Visibilities(output_directory, btm_directory, maps_tag, map_filepaths)
-#vis.get_visibilities()
+vis = obs.Visibilities(output_directory, btm_directory, maps_tag, map_filepaths)
+vis.get_visibilities()
 
 ############# adding noise and calibration errors ##############
 
-print('adding errors')
-
-amplitude_errors_filepath = '/home/rebeccac/scratch/H-GASP/notebooks/visibility_amplitude_errors.npy'
-phase_errors_filepath = '/home/rebeccac/scratch/H-GASP/notebooks/visibility_phase_errors.npy'
+amplitude_errors_filepath = '/home/rebeccac/scratch/H_GASP/tutorials/visibility_amplitude_errors.npy'
+phase_errors_filepath = '/home/rebeccac/scratch/H_GASP/tutorials/visibility_phase_errors.npy'
 
 real_vis_obs = obs.RealisticVisibilities(ndays, btm_directory, output_directory, maps_tag)    
 real_vis = real_vis_obs.add_noise_calibration_errors(amplitude_errors_filepath,
@@ -116,8 +107,6 @@ real_vis = real_vis_obs.add_noise_calibration_errors(amplitude_errors_filepath,
 
 ############# getting dirty map #################
 
-print('getting dirty map')
-
 dirty_map_filename = 'dirty_map.h5'
 
 dm = obs.DirtyMap(real_vis, 
@@ -125,8 +114,5 @@ dm = obs.DirtyMap(real_vis,
                   fstate, nside, btm_directory)
 
 dm.get_dirty_map()
-
-print('done')
-
 
 ######## DONE ###########
